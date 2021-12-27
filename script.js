2697617450b2b9a67ea69b4fffb1428df29a2927
Kerry Kreiter <43457152+comm-scripter@users.noreply.github.com>
2021-12-27 13:00:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 pixelBtn = document.getElementById("btn");
+xBtn = document.getElementById("xBtn");
 ratio = window.devicePixelRatio
+dimensionX = window.screenX
 
 pixelBtn.addEventListener("click", function() {
     console.log("button clicked");
     alert("Your device pixel ratio is: " + ratio);
 });
-    
+
+xBtn.addEventListener("click", function() {
+    console.log("button clicked");
+    alert("Your screen resolution is: " + screen.width + "x" + screen.height);
+});
